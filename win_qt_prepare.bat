d1a75b45fd05479ce02015dd91922419a7b9f394
loudmannn <zteblade777@gmail.com>
2020-06-17 12:09:43 +0300

    fix загрузки QT

--- win_qt_prepare.bat
+++ win_qt_prepare.bat
@@ -1,8 +1,51 @@
 @echo off
 chcp 866 >nul
-set PATH=%cd%\tools\Qt\Qt5.11.1\5.11.1\bin;%cd%\tools\wget\bin;%cd%\tools\7-Zip;%cd%\tools\Qt\Qt5.11.1\Tools\mingw530_32\bin;%PATH%
+set PATH=%cd%\tools\Qt\Qt5.11.1\5.11.1\bin;%cd%\tools\wget\bin;%cd%\tools\7-Zip;%PATH%
 
 echo Данный скрипт скачивает и настраивает Qt5 для сборки проекта...
+
+echo Скачивание Qt5...
+
+SET downloadUrl=http://mechsys.ru/Qt.7z
+SET zipFile=%~dp0tools\Qt-Static.7z
+
+set /A maxbytesize=1606852212
+::set /A maxbytesize=1606762350
+set /A minbytesize=0
+IF EXIST %zipFile% (
+	call :setsize %zipFile%
+) ELSE (
+	set /A size=0
+)
+
+::echo %size%
+
+IF %size% EQU %minbytesize% (
+	wget --no-check-certificate -O %zipFile% %downloadUrl% 
+) ELSE (
+	IF %size% LSS %maxbytesize% (
+		wget --no-check-certificate --continue -O %zipFile% %downloadUrl% 
+	) ELSE (
+		echo Архив с Qt уже скачан...
+	)
+)
+
+IF %size% EQU %maxbytesize% (
+	IF EXIST "%~dp0tools\Qt\Qt5.11.1\5.11.1\bin" (
+		echo Архив уже распакован...
+	) ELSE (
+		echo Распаковка Qt5...
+		7z x %zipFile% -o%~dp0tools
+		IF EXIST "%~dp0tools\Qt\Qt5.11.1\5.11.1\bin" (
+			echo Распаковка Завершена...
+		) ELSE (
+			echo Не удалось распаковать архив, удалите %zipFile% и запустите этот скрипт заново...
+		)
+	)
+) ELSE (
+	echo %zipFile% поврежден, удалите его и запустите этот скрипт заново
+)
+
 
 echo Настройка окружения...
 ::echo %~dp0
